429979f04d5c6347983ecd9adfb1999d2588e324
JamesATom <abduhakimovabdulaziz7@gmail.com>
2025-05-13 22:42:28 +0500

    feat: Improve .gitlab-ci.yml to cancel previous builds and deploys when multiple commits

--- apps/gateway/src/common/guards/auth/ws-auth.guard.ts
+++ apps/gateway/src/common/guards/auth/ws-auth.guard.ts
@@ -1,38 +1,38 @@
-// ws-auth.guard.ts
-import {
-    CanActivate,
-    ExecutionContext,
-    Injectable,
-    Logger,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { Socket } from 'socket.io';
-import { WsException } from '@nestjs/websockets';
-import { JwtPayload, AuthenticatedSocket } from '../../types/types';
+// // ws-auth.guard.ts
+// import {
+//     CanActivate,
+//     ExecutionContext,
+//     Injectable,
+//     Logger,
+// } from '@nestjs/common';
+// import { JwtService } from '@nestjs/jwt';
+// import { Socket } from 'socket.io';
+// import { WsException } from '@nestjs/websockets';
+// import { JwtPayload, AuthenticatedSocket } from '../../types/types';
 
-@Injectable()
-export class WsJwtAuthGuard implements CanActivate {
-    private readonly logger = new Logger(WsJwtAuthGuard.name);
+// @Injectable()
+// export class WsJwtAuthGuard implements CanActivate {
+//     private readonly logger = new Logger(WsJwtAuthGuard.name);
 
-    constructor(private readonly jwtService: JwtService) {}
+//     constructor(private readonly jwtService: JwtService) {}
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        const client: AuthenticatedSocket = context.switchToWs().getClient<AuthenticatedSocket>();
-        const token = this.extractTokenFromClient(client);
-        if (!token) {
-            throw new WsException('Token not found');
-        }
+//     async canActivate(context: ExecutionContext): Promise<boolean> {
+//         const client: AuthenticatedSocket = context.switchToWs().getClient<AuthenticatedSocket>();
+//         const token = this.extractTokenFromClient(client);
+//         if (!token) {
+//             throw new WsException('Token not found');
+//         }
 
-        try {
-            client.user = await this.jwtService.verifyAsync(token);
-            return true;
-        } catch (err: any) {
-            this.logger.error(err.message);
-            throw new WsException('Invalid token');
-        }
-    }
+//         try {
+//             client.user = await this.jwtService.verifyAsync(token);
+//             return true;
+//         } catch (err: any) {
+//             this.logger.error(err.message);
+//             throw new WsException('Invalid token');
+//         }
+//     }
 
-    private extractTokenFromClient(client: AuthenticatedSocket): string | null {
-        return ((client?.handshake?.query?.token as string) || client?.handshake?.auth?.token?.split(' ')[1] || null);
-    }
-}
+//     private extractTokenFromClient(client: AuthenticatedSocket): string | null {
+//         return ((client?.handshake?.query?.token as string) || client?.handshake?.auth?.token?.split(' ')[1] || null);
+//     }
+// }
